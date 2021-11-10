497d1cbc7e29fed6496a996094a6bdd0784b480b
Alejandro <alexrios182@gmail.com>
2021-11-09 22:42:27 -0600

    add basic login route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
