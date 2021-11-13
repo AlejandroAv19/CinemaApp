c62ab3794fbb44c3fe5d5d8a38b6216c8d6f8ad0
Alejandro <alexrios182@gmail.com>
2021-11-13 12:34:11 -0600

    reorganize routes with router.route

--- routes/employees.js
+++ routes/employees.js
@@ -1,18 +1,24 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const router = express.Router();
 
+router.use(methodOverride("_method"));
 router.use(express.urlencoded({ extended: true }));
 
 // CONTROLLERS
 const employees = require("../controllers/employees");
 
 // ROUTES
-router.get("/", employees.index);
-router.post("/", employees.create);
+router.route("/").get(employees.index).post(employees.create);
+
 router.get("/new", employees.newForm);
-router.get("/:id", employees.show);
+
+router
+  .route("/:id")
+  .get(employees.show)
+  .patch(employees.update)
+  .delete(employees.delete);
+
 router.get("/:id/edit", employees.updateForm);
-router.patch("/:id", employees.update);
-router.delete("/:id", employees.delete);
 
 module.exports = router;
