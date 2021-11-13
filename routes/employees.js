b1d1466aa16fd1c1659150fa58b883e400dad6e1
Alejandro <alexrios182@gmail.com>
2021-11-13 12:22:49 -0600

    modify routes to use controller

--- routes/employees.js
+++ routes/employees.js
@@ -1,60 +1,18 @@
 const express = require("express");
-const methodOverride = require("method-override");
-
 const router = express.Router();
 
 router.use(express.urlencoded({ extended: true }));
-router.use(methodOverride("_method"));
 
-const Employee = require("../models/employee");
+// CONTROLLERS
+const employees = require("../controllers/employees");
 
-// EMPLOYEE ROUTES
-// Index   | /employees          | GET    | Display all comments
-router.get("/", async (req, res) => {
-  const employees = await Employee.find({});
-  res.render("employees/index", { employees });
-});
-
-// New     | /employees/new      | GET    | Form to create a new employee
-router.get("/new", (req, res) => {
-  res.render("employees/new");
-});
-
-// Create  | /employees          | POST   | Creates new employee
-router.post("/", async (req, res) => {
-  const newEmployee = new Employee(req.body);
-  await newEmployee.save();
-  res.redirect("/");
-});
-
-// Show    | /employees/:id      | GET    | Details for one specific employee
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const employee = await Employee.findById(id);
-  res.render("employees/show", { employee });
-});
-
-// Edit    | /employees/:id/edit | GET    | Form to edit specific employee
-router.get("/:id/edit", async (req, res) => {
-  const id = req.params.id;
-  const employee = await Employee.findById(id);
-  res.render("employees/edit", { employee });
-});
-
-// Update  | /employee/:id       | PATCH  | Updates specific employee
-router.patch("/:id", async (req, res) => {
-  const id = req.params.id;
-  const name = req.body.name;
-  const age = req.body.age;
-  await Employee.findByIdAndUpdate(id, { name, age });
-  res.redirect("/");
-});
-
-// Destroy | /employee/:id       | DELETE | Deletes specific item
-router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  await Employee.findByIdAndDelete(id);
-  res.redirect("/");
-});
+// ROUTES
+router.get("/", employees.index);
+router.post("/", employees.create);
+router.get("/new", employees.newForm);
+router.get("/:id", employees.show);
+router.get("/:id/edit", employees.updateForm);
+router.patch("/:id", employees.update);
+router.delete("/:id", employees.delete);
 
 module.exports = router;
