4a673779a839897f993c3024fb8b9a28f9f41342
Alejandro <alexrios182@gmail.com>
2021-11-13 11:59:27 -0600

    estructure employees route file and clean app.js file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// NPM PACKAGES
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
@@ -7,6 +8,11 @@ const methodOverride = require("method-override");
 const app = express();
 dotenv.config();
 
+// EXPRESS ROUTING
+const employeeRoutes = require("./routes/employees");
+app.use("/employees", employeeRoutes);
+
+// ESPRESS SET & USE
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -24,9 +30,6 @@ mongoose
     console.log("error", e);
   });
 
-// DB MODELS
-const Employee = require("./models/employee");
-
 // ROUTES
 app.get("/", (req, res) => {
   res.render("home");
@@ -34,55 +37,6 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.render("login");
-});
-
-// EMPLOYEE ROUTES
-// Index   | /employees          | GET    | Display all comments
-app.get("/employees", async (req, res) => {
-  const employees = await Employee.find({});
-  res.render("employees/index", { employees });
-});
-
-// New     | /employees/new      | GET    | Form to create a new employee
-app.get("/employees/new", (req, res) => {
-  res.render("employees/new");
-});
-
-// Create  | /employees          | POST   | Creates new employee
-app.post("/employees", async (req, res) => {
-  const newEmployee = new Employee(req.body);
-  await newEmployee.save();
-  res.redirect("/");
-});
-
-// Show    | /employees/:id      | GET    | Details for one specific employee
-app.get("/employees/:id", async (req, res) => {
-  const id = req.params.id;
-  const employee = await Employee.findById(id);
-  res.render("employees/show", { employee });
-});
-
-// Edit    | /employees/:id/edit | GET    | Form to edit specific employee
-app.get("/employees/:id/edit", async (req, res) => {
-  const id = req.params.id;
-  const employee = await Employee.findById(id);
-  res.render("employees/edit", { employee });
-});
-
-// Update  | /employee/:id       | PATCH  | Updates specific employee
-app.patch("/employees/:id", async (req, res) => {
-  const id = req.params.id;
-  const name = req.body.name;
-  const age = req.body.age;
-  await Employee.findByIdAndUpdate(id, { name, age });
-  res.redirect("/");
-});
-
-// Destroy | /employee/:id       | DELETE | Deletes specific item
-app.delete("/employees/:id", async (req, res) => {
-  const id = req.params.id;
-  await Employee.findByIdAndDelete(id);
-  res.redirect("/");
 });
 
 // MISC ROUTES
