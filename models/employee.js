bcb365c3aade9561d4d2fd1a33f32dd75a01ca4e
Alejandro <alexrios182@gmail.com>
2021-11-13 19:46:48 -0600

    update employee model

--- models/employee.js
+++ models/employee.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const employeeSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
+  name: { type: String, required: true },
+  hireDate: { type: Date, default: Date.now, required: true },
+  userType: {
+    type: String,
+    emun: ["Employee", "Admin", "Stock Manager"],
+    required: true,
+  },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Employee", employeeSchema);
