ce9e39947ffcb061706f83008ee124a5ae3383f4
Alejandro <alexrios182@gmail.com>
2021-11-15 18:19:38 -0600

    create movie model

---
+++ models/movie.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  year: { type: Number, required: true },
+  sinopsis: { type: String, required: true },
+  rating: { type: String, required: true },
+  language: { type: String, required: true },
+  duraion: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Movie", movieSchema);
