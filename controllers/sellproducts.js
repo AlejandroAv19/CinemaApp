6b6112ef4c132e7deca5834d57ad393808a2e46a
Alejandro <alexrios182@gmail.com>
2021-11-18 23:26:36 -0600

    add home controller

--- controllers/sellproducts.js
+++ controllers/sellproducts.js
@@ -1,3 +1,5 @@
-module.exports.home = (req, res) => {
-  res.render("sellproducts/home");
+const Product = require("../models/product");
+module.exports.home = async (req, res) => {
+  const products = await Product.find({});
+  res.render("sellproducts/home", { products });
 };
