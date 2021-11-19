0606ae1095e9ab501f8675eb7b5a15bde133a1c3
Alejandro <alexrios182@gmail.com>
2021-11-18 18:30:01 -0600

    update provider model to store products

--- models/provider.js
+++ models/provider.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const providerSchema = new mongoose.Schema({
+const providerSchema = new Schema({
   name: { type: String, required: true },
   phone: { type: Number, required: true },
   email: { type: String, required: true },
+  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
 });
 
 module.exports = mongoose.model("Provider", providerSchema);
