e5cd843a1043dc0390325006ff3d6ab9bb1ab838
Alejandro <alexrios182@gmail.com>
2021-11-20 17:23:25 -0600

    add create working product purchase logic

--- controllers/sellproducts.js
+++ controllers/sellproducts.js
@@ -1,5 +1,41 @@
 const Product = require("../models/product");
+const ProductPurchase = require("../models/productPurchase");
+
 module.exports.home = async (req, res) => {
   const products = await Product.find({});
   res.render("sellproducts/home", { products });
+};
+
+module.exports.create = async (req, res) => {
+  // RETRIEVING THE INFORMATION FROM THE PURCHASE
+  const { name, quantity, subtotal } = req.body;
+  // RETRIEVING LENGHT TO CHECK IF IT IS AN ARRAY OR NOT | HOW MANY TIMES WE NEED TO ITERATE
+  const length = name.length;
+  // CREATING OUR PURCHASE
+  const purchase = new ProductPurchase({});
+  // IF THERE IS ONLY ONE ELEMENT
+  if (typeof name === "string") {
+    // FIND THE PRODUCT
+    const item = await Product.findOne({ name });
+    // PUSH THE ELEMENT TO THE ELEMENTS ARRAY OF THE PURCHASE
+    purchase.items.push({ item, quantity, subtotal });
+    // SAVE THE DOCUMENT
+    await purchase.save();
+  } else {
+    // IF THERE IS MORE THAN ONE ELEMENT
+    for (let i = 0; i < length; i++) {
+      // FINDING THE PRODUCT
+      const item = await Product.findOne({ name: name[i] });
+      // PUSHING THE ITEMS TO THE ARRAY IN PURCHASE
+      purchase.items.push({
+        item,
+        quantity: quantity[i],
+        subtotal: subtotal[i],
+      });
+    }
+    // ONCE FINISHED PUSHEN SAVE THE DOCUMENT
+    await purchase.save();
+    //RECEIPT CREATION
+  }
+  res.redirect("/home");
 };
