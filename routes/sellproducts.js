f84bd8eae5db8a5eba3b3bb47f674235ec71ed19
Alejandro <alexrios182@gmail.com>
2021-11-20 20:28:41 -0600

    add show route

--- routes/sellproducts.js
+++ routes/sellproducts.js
@@ -8,5 +8,6 @@ router.use(express.urlencoded({ extended: true }));
 const sellproducts = require("../controllers/sellproducts");
 
 router.route("/").get(isLoggedIn, sellproducts.home).post(sellproducts.create);
+router.route("/:id").get(isLoggedIn, sellproducts.show);
 
 module.exports = router;
