0c15d7c665ed136c1d1c9f84623f73829c1a221e
Alejandro <alexrios182@gmail.com>
2021-11-20 19:20:13 -0600

    rename receipt to sale

--- models/sale.js
+++ models/sale.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const receiptSchema = new Schema({
+const saleSchema = new Schema({
+  details: { type: Schema.Types.ObjectId, ref: "ProductPurchase" },
   madeBy: { type: Schema.Types.ObjectId, ref: "User" },
   type: { type: String, enum: ["product", "ticket"] },
   date: { type: String },
   total: { type: Number },
 });
 
-module.exports = mongoose.model("Receipt", receiptSchema);
+module.exports = mongoose.model("Sale", saleSchema);
 
 // Empleado | Tipo (Producto | Ticket) | Fecha | Total | Detalles
