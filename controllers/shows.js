61955400e5eacee7fe6c70bbb6f1d477389e529d
Alejandro <alexrios182@gmail.com>
2021-11-23 00:47:37 -0600

    create show controller

---
+++ controllers/shows.js
@@ -0,0 +1,23 @@
+const Movie = require("../models/movie");
+const Show = require("../models/show");
+
+const Auditorium = require("../models/auditorium");
+module.exports.home = async (req, res) => {
+  const movies = await Movie.find({});
+  const auditoriums = await Auditorium.find({});
+  res.render("shows/home", { movies, auditoriums });
+};
+
+module.exports.create = async (req, res) => {
+  const { movie, auditorium, day, showtime, availableSeats } = req.body;
+  // CREATING SHOW
+  const newShow = new Show({
+    movie,
+    auditorium,
+    day,
+    showtime,
+    availableSeats,
+  });
+  await newShow.save();
+  res.send(newShow);
+};
