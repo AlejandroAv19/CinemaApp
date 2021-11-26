5736a11aeaf3dca3547f127e5fd8621a8830bd4d
Alejandro <alexrios182@gmail.com>
2021-11-26 16:23:09 -0600

    update logic due to unmet expectations

--- public/scripts/productorders.js
+++ public/scripts/productorders.js
@@ -39,20 +39,133 @@ confirmQuantityButton.addEventListener("click", function () {
   ).innerHTML;
   const subtotalOverview = document.querySelector("#subtotal_overview");
   subtotalOverview.innerHTML = parseInt(quantity) * parseInt(pricePerUnit);
-  // PROVIDERS CUT
-  const providersPercentage = 0.15;
-  const providersCut =
-    parseInt(subtotalOverview.innerHTML) * providersPercentage;
-  const providersCutOverview = document.querySelector("#provider_cut_overview");
-  providersCutOverview.innerHTML = providersCut;
-  // TOTAL
-  const total =
-    parseFloat(subtotalOverview.innerHTML) +
-    parseFloat(providersCutOverview.innerHTML);
-  const totalOverview = document.querySelector("#total_overview");
-  totalOverview.innerHTML = total;
 });
 
+// ADD TO ORDER
+const addToOrderButton = document.querySelector("#add_to_order_button");
+
+// ADD THE PRODUCT TO THE ORDER
+addToOrderButton.addEventListener("click", function () {
+  const tableBody = document.querySelector("#order_table_body");
+  const productNameID = document
+    .querySelector("#product_overview")
+    .innerHTML.replaceAll(" ", "_");
+
+  const productName = document.querySelector("#product_overview");
+  const quantity = document.querySelector("#quantity_overview");
+  const PPU = document.querySelector("#price_per_unit_overview");
+  const subtotal = document.querySelector("#subtotal_overview");
+
+  // IF THE ROW IS ALREADY THERE
+  const rowAvailable = document.querySelector(
+    "#row_order_overview_" + productNameID
+  );
+
+  if (!rowAvailable) {
+    const row = document.createElement("tr");
+    row.id = "row_order_overview_" + productNameID;
+
+    // PRODUCT NAME
+    const productTD = document.createElement("td");
+    productTD.innerHTML = productName.innerHTML;
+    row.appendChild(productTD);
+
+    // QUANTITY
+    const quantityTD = document.createElement("td");
+    quantityTD.innerHTML = quantity.innerHTML;
+    row.appendChild(quantityTD);
+
+    // PPU
+    const PPUTD = document.createElement("td");
+    PPUTD.innerHTML = PPU.innerHTML;
+    row.appendChild(PPUTD);
+
+    // SUBTOTAL
+    const subtotalTD = document.createElement("td");
+    subtotalTD.innerHTML = subtotal.innerHTML;
+    row.appendChild(subtotalTD);
+
+    tableBody.appendChild(row);
+
+    // CLEANING THE OVERVIEW TABLE
+    productName.innerHTML = "";
+    quantity.innerHTML = "";
+    PPU.innerHTML = "";
+    subtotal.innerHTML = "";
+
+    // ADD VALUES TO TOTALS
+    // SUBTOTAL
+    const totalsSubtotal = document.querySelector("#totals_subtotal");
+    totalsSubtotal.innerHTML =
+      parseFloat(totalsSubtotal.innerHTML) + parseFloat(subtotalTD.innerHTML);
+
+    // PROVIDERS CUT
+    const totalsProvidersCut = document.querySelector("#totals_providers_cut");
+    const cut = (parseFloat(totalsSubtotal.innerHTML) * 0.1).toFixed(1);
+    totalsProvidersCut.innerHTML = cut;
+
+    // IVA
+    const totalsIVA = document.querySelector("#totals_iva");
+    const iva = (parseFloat(totalsSubtotal.innerHTML) * 0.05).toFixed(1);
+    totalsIVA.innerHTML = iva;
+
+    // TOTAL
+    const totalsTotal = document.querySelector("#totals_total");
+    const total =
+      parseFloat(totalsSubtotal.innerHTML) +
+      parseFloat(totalsProvidersCut.innerHTML) +
+      parseFloat(totalsIVA.innerHTML);
+    totalsTotal.innerHTML = total.toFixed(1);
+  } else {
+    const productRow = document.querySelector(
+      "#row_order_overview_" + productNameID
+    );
+    const rowChilds = productRow.childNodes;
+
+    // QUANTITY
+    const overviewQuantity =
+      document.querySelector("#quantity_overview").innerHTML;
+    const orderQuantity = rowChilds[1].innerHTML;
+    rowChilds[1].innerHTML =
+      parseInt(overviewQuantity) + parseInt(orderQuantity);
+
+    // Subtotal
+    const overviewSubtotal =
+      document.querySelector("#subtotal_overview").innerHTML;
+    const orderSubtotal = rowChilds[3].innerHTML;
+    rowChilds[3].innerHTML =
+      parseInt(overviewSubtotal) + parseInt(orderSubtotal);
+
+    // CLEANING THE OVERVIEW TABLE
+    productName.innerHTML = "";
+    quantity.innerHTML = "";
+    PPU.innerHTML = "";
+    subtotal.innerHTML = "";
+
+    // ADD VALUES TO TOTALS
+    // SUBTOTAL
+    const totalsSubtotal = document.querySelector("#totals_subtotal");
+    totalsSubtotal.innerHTML = 0;
+    const orderTable = document.querySelector("#order_table_body");
+    const tableEl = orderTable.childNodes;
+    for (el of tableEl) {
+      const data = el.childNodes;
+      totalsSubtotal.innerHTML =
+        parseInt(totalsSubtotal.innerHTML) + parseInt(data[3].innerHTML);
+    }
+    // PROVIDERS CUT
+    const totalsProvidersCut = document.querySelector("#totals_providers_cut");
+    const cut = (parseInt(totalsSubtotal.innerHTML) * 0.1).toFixed(1);
+    totalsProvidersCut.innerHTML = cut;
+
+    // IVA
+    const totalsIVA = document.querySelector("#totals_iva");
+    const iva = (parseInt(totalsSubtotal.innerHTML) * 0.05).toFixed(1);
+    totalsIVA.innerHTML = iva;
+  }
+});
+
+/*
 // CONFIRM PURCHASE
 const purchaseButton = document.querySelector("#confirm_purchase_button");
 purchaseButton.addEventListener("click", function () {
@@ -85,3 +198,4 @@ purchaseButton.addEventListener("click", function () {
   const totalForm = document.querySelector("#total_form");
   totalForm.value = total;
 });
+*/
