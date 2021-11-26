3c306aba6bdca8de02234890ce81a824549f4d27
Alejandro <alexrios182@gmail.com>
2021-11-26 16:21:14 -0600

    add logic to run custom error handler when 401

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,10 @@
+// CUSTOM ERROR
+const CustomError = require("../config/CustomError");
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.flash("error", "You must be signed in...");
-    return res.redirect("/");
+    throw new CustomError(401, "Unauthorized");
   }
   next();
 };
