ad74fda2279b9ce03c901e259d6bec1e646f59ed
Alejandro <alexrios182@gmail.com>
2021-11-26 20:54:03 -0600

    replace sellproducts scripts with the ones at order products

--- public/scripts/sellproducts.js
+++ public/scripts/sellproducts.js
@@ -1,139 +1,154 @@
-const addProduct = (name, price) => {
-  const nameReplace = name.replaceAll(" ", "_");
-  const table = document.querySelector(".overview_table_body");
-  // PRIMER ELEMENTO
-  if (!table.hasChildNodes()) {
-    // PRIMER ELEMENTO
-    // CREANDO ELEMENTOS
+// PRODUCT SELECTION
+const addProduct = (productName, pricePerUnit) => {
+  const productOverview = document.querySelector("#product_overview");
+  const pricePerUnitOverview = document.querySelector(
+    "#price_per_unit_overview"
+  );
+  productOverview.innerHTML = productName;
+  pricePerUnitOverview.innerHTML = pricePerUnit;
+};
+
+// ADD QUANTITY BUTTON
+const addQuantity = document.querySelector("#add_quantity_button");
+addQuantity.addEventListener("click", function () {
+  const numberDisplay = document.querySelector("#product_quantity");
+  numberDisplay.value = parseInt(numberDisplay.value) + 1;
+});
+
+// SUBSTRACT QUANTITY BUTTON
+const substractQuantity = document.querySelector("#substract_quantity_button");
+substractQuantity.addEventListener("click", function () {
+  const numberDisplay = document.querySelector("#product_quantity");
+  if (numberDisplay.value != "1") {
+    numberDisplay.value = parseInt(numberDisplay.value) - 1;
+  }
+});
+
+// CONFIRM QUANTITY
+const confirmQuantityButton = document.querySelector(
+  "#confirm_quantity_button"
+);
+confirmQuantityButton.addEventListener("click", function () {
+  // ADD QUANTITY TO OVERVIEW
+  const quantity = document.querySelector("#product_quantity").value;
+  const quantityOverview = document.querySelector("#quantity_overview");
+  quantityOverview.innerHTML = quantity;
+  // SUBTOTAL
+  const pricePerUnit = document.querySelector(
+    "#price_per_unit_overview"
+  ).innerHTML;
+  const subtotalOverview = document.querySelector("#subtotal_overview");
+  subtotalOverview.innerHTML = parseInt(quantity) * parseInt(pricePerUnit);
+});
+
+// ADD TO ORDER
+const addToOrderButton = document.querySelector("#add_to_order_button");
+// ADD THE PRODUCT TO THE ORDER
+addToOrderButton.addEventListener("click", function () {
+  const tableBody = document.querySelector("#order_table_body");
+  const productNameID = document
+    .querySelector("#product_overview")
+    .innerHTML.replaceAll(" ", "_");
+
+  const productName = document.querySelector("#product_overview");
+  const quantity = document.querySelector("#quantity_overview");
+  const PPU = document.querySelector("#price_per_unit_overview");
+  const subtotal = document.querySelector("#subtotal_overview");
+
+  // IF THE ROW IS ALREADY THERE
+  const rowAvailable = document.querySelector(
+    "#row_order_overview_" + productNameID
+  );
+
+  if (!rowAvailable) {
     const row = document.createElement("tr");
-    row.id = nameReplace;
-    let td_product_name = document.createElement("td");
-    let td_quantity = document.createElement("td");
-    td_quantity.id = nameReplace + "_quantity";
-    let td_subtotal = document.createElement("td");
-    td_subtotal.id = nameReplace + "_subtotal";
-    // LLENANDO LA INFORMACION
-    td_product_name.innerHTML = name;
-    if (td_quantity.innerHTML === "") {
-      td_quantity.innerHTML = 1;
-    } else {
-      td_quantity.innerHTML = parseInt(td_quantity.innerHTML) + 1;
-    }
-    td_subtotal.innerHTML = parseInt(td_quantity.innerHTML) * parseInt(price);
-    // APPENDING THE ELEMENTS
-    row.appendChild(td_product_name);
-    row.appendChild(td_quantity);
-    row.appendChild(td_subtotal);
-    table.appendChild(row);
-    // ADDING TO TOTAL
-    let totalDisplay = document.querySelector("#total");
-    totalDisplay.innerHTML = parseInt(totalDisplay.innerHTML) + parseInt(price);
+    row.id = "row_order_overview_" + productNameID;
+
+    // PRODUCT NAME
+    const productTD = document.createElement("td");
+    productTD.innerHTML = productName.innerHTML;
+    row.appendChild(productTD);
+
+    // QUANTITY
+    const quantityTD = document.createElement("td");
+    quantityTD.innerHTML = quantity.innerHTML;
+    row.appendChild(quantityTD);
+
+    // PPU
+    const PPUTD = document.createElement("td");
+    PPUTD.innerHTML = PPU.innerHTML;
+    row.appendChild(PPUTD);
+
+    // SUBTOTAL
+    const subtotalTD = document.createElement("td");
+    subtotalTD.innerHTML = subtotal.innerHTML;
+    row.appendChild(subtotalTD);
+
+    tableBody.appendChild(row);
+
+    // CLEANING THE OVERVIEW TABLE
+    productName.innerHTML = "";
+    quantity.innerHTML = "";
+    PPU.innerHTML = "";
+    subtotal.innerHTML = "";
+
+    // ADD VALUES TO TOTALS
+    // SUBTOTAL
+    const totalsSubtotal = document.querySelector("#totals_subtotal");
+    totalsSubtotal.innerHTML =
+      parseFloat(totalsSubtotal.innerHTML) + parseFloat(subtotalTD.innerHTML);
+
+    // IVA
+    const totalsIVA = document.querySelector("#totals_iva");
+    const iva = (parseFloat(totalsSubtotal.innerHTML) * 0.05).toFixed(1);
+    totalsIVA.innerHTML = iva;
+
+    // TOTAL
+    const totalsTotal = document.querySelector("#totals_total");
+    const total =
+      parseFloat(totalsSubtotal.innerHTML) + parseFloat(totalsIVA.innerHTML);
+    totalsTotal.innerHTML = total.toFixed(1);
   } else {
-    // SI LA FILA YA EXISTE
-    if (document.querySelector(`#${nameReplace}`)) {
-      let selectedQuantity = document.querySelector(`#${nameReplace}_quantity`);
-      selectedQuantity.innerHTML = parseInt(selectedQuantity.innerHTML) + 1;
-      let selectedSubtotal = document.querySelector(`#${nameReplace}_subtotal`);
-      selectedSubtotal.innerHTML = parseInt(selectedQuantity.innerHTML) * price;
-      // ADDING TO TOTAL
-      let totalDisplay = document.querySelector("#total");
-      totalDisplay.innerHTML =
-        parseInt(totalDisplay.innerHTML) + parseInt(price);
-    } else {
-      // SI NO EXISTE SE CREA
-      const row = document.createElement("tr");
-      row.id = nameReplace;
-      let td_product_name = document.createElement("td");
-      let td_quantity = document.createElement("td");
-      td_quantity.id = nameReplace + "_quantity";
-      let td_subtotal = document.createElement("td");
-      td_subtotal.id = nameReplace + "_subtotal";
-      // LLENANDO LA INFORMACION
-      td_product_name.innerHTML = name;
-      if (td_quantity.innerHTML === "") {
-        td_quantity.innerHTML = 1;
-      } else {
-        td_quantity.innerHTML = parseInt(td_quantity.innerHTML) + 1;
-      }
-      td_subtotal.innerHTML = parseInt(td_quantity.innerHTML) * parseInt(price);
-      // APPENDING THE ELEMENTS
-      row.appendChild(td_product_name);
-      row.appendChild(td_quantity);
-      row.appendChild(td_subtotal);
-      table.appendChild(row);
-      // ADDING TO TOTAL
-      let totalDisplay = document.querySelector("#total");
-      totalDisplay.innerHTML =
-        parseInt(totalDisplay.innerHTML) + parseInt(price);
+    const productRow = document.querySelector(
+      "#row_order_overview_" + productNameID
+    );
+    const rowChilds = productRow.childNodes;
+
+    // QUANTITY
+    const overviewQuantity =
+      document.querySelector("#quantity_overview").innerHTML;
+    const orderQuantity = rowChilds[1].innerHTML;
+    rowChilds[1].innerHTML =
+      parseInt(overviewQuantity) + parseInt(orderQuantity);
+
+    // Subtotal
+    const overviewSubtotal =
+      document.querySelector("#subtotal_overview").innerHTML;
+    const orderSubtotal = rowChilds[3].innerHTML;
+    rowChilds[3].innerHTML =
+      parseInt(overviewSubtotal) + parseInt(orderSubtotal);
+
+    // CLEANING THE OVERVIEW TABLE
+    productName.innerHTML = "";
+    quantity.innerHTML = "";
+    PPU.innerHTML = "";
+    subtotal.innerHTML = "";
+
+    // ADD VALUES TO TOTALS
+    // SUBTOTAL
+    const totalsSubtotal = document.querySelector("#totals_subtotal");
+    totalsSubtotal.innerHTML = 0;
+    const orderTable = document.querySelector("#order_table_body");
+    const tableEl = orderTable.childNodes;
+    for (el of tableEl) {
+      const data = el.childNodes;
+      totalsSubtotal.innerHTML =
+        parseInt(totalsSubtotal.innerHTML) + parseInt(data[3].innerHTML);
     }
+
+    // IVA
+    const totalsIVA = document.querySelector("#totals_iva");
+    const iva = (parseInt(totalsSubtotal.innerHTML) * 0.05).toFixed(1);
+    totalsIVA.innerHTML = iva;
   }
-};
-
-const removeProduct = (name, price) => {
-  const nameReplace = name.replaceAll(" ", "_");
-  let productRow = document.querySelector(`#${nameReplace}`);
-  if (productRow) {
-    let selectedQuantity = document.querySelector(`#${nameReplace}_quantity`);
-    let selectedSubtotal = document.querySelector(`#${nameReplace}_subtotal`);
-    selectedQuantity.innerHTML = parseInt(selectedQuantity.innerHTML) - 1;
-    selectedSubtotal.innerHTML =
-      parseInt(selectedQuantity.innerHTML) * parseInt(price);
-    // SUBSTRACTING TO TOTAL
-    let totalDisplay = document.querySelector("#total");
-    totalDisplay.innerHTML = parseInt(totalDisplay.innerHTML) - parseInt(price);
-    if (selectedQuantity.innerHTML == 0) {
-      productRow.remove();
-    }
-  }
-};
-
-const purchase = () => {
-  // TARGETING THE OVERVIEW TABLE
-  let overviewTable = document.querySelector(".overview_table_body");
-  // FOR EVERY ITEM IN THE OVERVIEW
-  for (item of overviewTable.childNodes) {
-    const item_name = item.childNodes[0].innerHTML;
-    const item_quantity = item.childNodes[1].innerHTML;
-    const item_subtotal = item.childNodes[2].innerHTML;
-
-    // CREATING THE ELEMENTS
-    const i1 = document.createElement("input");
-    const i2 = document.createElement("input");
-    const i3 = document.createElement("input");
-
-    //SETTING THE TYPE
-    i1.type = "text";
-    i2.type = "number";
-    i3.type = "number";
-
-    // SETTING THE NAME
-    i1.name = "name";
-    i2.name = "quantity";
-    i3.name = "subtotal";
-
-    // SETTING THE VALUE
-    i1.value = item_name;
-    i2.value = item_quantity;
-    i3.value = item_subtotal;
-
-    // HIDDENT THE ELEMENTS
-    i1.hidden = true;
-    i2.hidden = true;
-    i3.hidden = true;
-
-    //APPENDING THE ELEMENTS
-    let purchaseForm = document.querySelector("#form_purchase");
-    purchaseForm.appendChild(i1);
-    purchaseForm.appendChild(i2);
-    purchaseForm.appendChild(i3);
-  }
-  // RETRIEVING THE TOTAL
-  const total = document.querySelector("#total").innerHTML;
-  let i4 = document.createElement("input");
-  i4.type = "number";
-  i4.name = "total";
-  i4.value = total;
-  i4.hidden = true;
-  let purchaseForm = document.querySelector("#form_purchase");
-  purchaseForm.appendChild(i4);
-};
+});
