f221548bacb286a038a932ed517557df9e73817d
Alejandro <alexrios182@gmail.com>
2021-12-02 09:20:42 -0600

    update sales controller to send order and products models

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,5 +1,9 @@
 const Sale = require("../models/sale");
+const Order = require("../models/order");
+const Product = require("../models/product");
 module.exports.home = async (req, res) => {
   const sales = await Sale.find({}).populate("madeBy").populate("details");
-  res.render("sales/home", { sales });
+  const orders = await Order.find({}).populate("madeBy").populate("details");
+  const products = await Product.find({});
+  res.render("sales/home", { sales, orders, products });
 };
