02bbb9fd4460fa624715ba0aba3d42e6af9f29ad
Alejandro <alexrios182@gmail.com>
2021-12-02 09:20:05 -0600

    update productorders with new functionality

--- controllers/productOrders.js
+++ controllers/productOrders.js
@@ -1,55 +1,84 @@
+// REQUIRED MODELS
 const Product = require("../models/product");
 const ProductOrder = require("../models/productOrder");
-const sale = require("../models/sale");
+const Order = require("../models/order");
 const User = require("../models/user");
+const OrderInvoice = require("../models/order_invoice_counter");
 
+// HOME RENDER
 module.exports.home = async (req, res) => {
   const products = await Product.find({});
-  res.render("productorders/home", { products });
+  const invoiceCounter = await OrderInvoice.find({});
+  // ---
+  const a = JSON.stringify(invoiceCounter);
+  const b = JSON.parse(a);
+  // ---
+  const nextInvoice = parseInt(b[0].seq_value) + 1;
+  res.render("productorders/home", {
+    products,
+    nextInvoice,
+  });
 };
 
 module.exports.create = async (req, res) => {
-  // -- PRODUCT ORDER
-  // FINDING THE PRODUCT
-  const productName = req.body.product;
-  const { quantity, pricePerUnit, subtotal, providersCut, total } = req.body;
-  const product = await Product.findOne({ name: productName });
-  // CREATING THE PRODUCT ORDER
-  const productOrder = new ProductOrder({
-    product,
-    quantity,
-    pricePerUnit,
-    subtotal,
-    providersCut,
-    total,
-  });
+  const { _product, quantity, pricePerUnit, date, invoice } = req.body;
+  console.log(req.body);
 
-  // -- SALE
-  const user = await User.findOne({ username: req.cookies.username });
-  const newSale = new sale({
-    details: productOrder,
-    madeBy: user,
-    type: "productOrder",
-    date: Date.now(),
-    total,
-  });
+  // NEW PRODUCT ORDER
+  const productOrder = new ProductOrder({ date });
 
-  // ADDING THE PRODUCT TO THE STOCK
-  const actualStock = product.stock;
-  const newStock = parseInt(actualStock) + parseInt(quantity);
+  if (Array.isArray(_product)) {
+    for (let i = 0; i < _product.length; i++) {
+      const _name = _product[i];
+      const _quantity = quantity[i];
+      const _ppu = pricePerUnit[i];
 
-  // IF THE NEW STOCK DOESNT GET ABOVE THE LIMIT
-  if (newStock <= parseInt(product.stockLimit)) {
-    const productUpdate = await Product.findOneAndUpdate(
-      { name: productName },
-      { stock: newStock }
-    );
-    await newSale.save();
+      // SEARCHING FOR THE PRODUCT
+      const productObject = await Product.findOne({ name: _name });
+
+      // productId
+      const a = JSON.stringify(productObject);
+      const b = JSON.parse(a);
+
+      const product = {
+        product: productObject,
+        productId: b.productId,
+        quantity: _quantity,
+        pricePerUnit: _ppu,
+      };
+      productOrder.products.push(product);
+      // ADDING THE PRODUCT TO THE STOCK
+      // FINDING THE PRODUCT
+      const addProduct = await Product.findOne({ name: _name });
+      const actualStock = addProduct.stock;
+      const newStock = parseInt(actualStock) + parseInt(_quantity);
+
+      // IF THE NEW STOCK DOESNT GET ABOVE THE LIMIT
+      if (newStock <= parseInt(addProduct.stockLimit)) {
+        const productUpdate = await Product.findOneAndUpdate(
+          { name: _name },
+          { stock: newStock }
+        );
+        await productUpdate.save();
+      }
+    }
+
     await productOrder.save();
+  } else {
+    console.log("NOT ARRAY");
   }
 
-  // REDIRECT
-  res.redirect("/home");
+  // FINDING THE USER
+  const user = await User.findOne({ username: req.cookies.username });
+
+  // CREATING THE ORDER
+  const newOrder = new Order({
+    details: productOrder,
+    madeBy: user,
+    date,
+  });
+
+  await newOrder.save();
 };
 
 module.exports.show = async (req, res) => {
