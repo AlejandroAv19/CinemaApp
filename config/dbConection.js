33beba3b8a38dd9858851dfdb9b5e2d9d0454489
Alejandro <alexrios182@gmail.com>
2021-12-02 09:16:57 -0600

    update db connection to return db object

--- config/dbConection.js
+++ config/dbConection.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
+const { db } = require("../models/product");
 // mongodb://localhost:27017/test
 module.exports.dbConnection = () => {
   mongoose
     .connect(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k8zct.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
     )
-    .then(() => {
+    .then((db) => {
       console.log("Database Connected");
+      return db;
     })
     .catch((e) => {
       console.log("error", e);
     });
+  return db;
 };
