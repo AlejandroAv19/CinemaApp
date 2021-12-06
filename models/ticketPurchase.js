470898cbe9c642c5aedd090a9954b812967915be
Alejandro <alexrios182@gmail.com>
2021-12-06 11:21:26 -0600

    remove total attribute and add showtime attribute

--- models/ticketPurchase.js
+++ models/ticketPurchase.js
@@ -3,10 +3,10 @@ const { Schema } = mongoose;
 
 const ticketPurchaseSchema = new Schema({
   movie: { type: String },
-  auditorium: { type: String },
-  day: { type: String, required: true },
+  auditorium: { type: Number },
+  day: { type: String },
+  showtime: { type: String },
   ticketsNumber: { type: Number, required: true },
-  total: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("TicketPurchase", ticketPurchaseSchema);
